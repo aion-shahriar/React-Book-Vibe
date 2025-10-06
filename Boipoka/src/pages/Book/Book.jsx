e459f3f6a12a79437740d3730daea0ec1347cbbc
Aion <aionshahriar23@gmail.com>
2025-10-07 05:34:22 +0600

    Book data load

---
+++ Boipoka/src/pages/Book/Book.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Book = () => {
+
+    // const data = use(bookPromise)
+    return (
+        <div>
+            <h1>Single Book</h1>
+        </div>
+    );
+};
+
+export default Book;
